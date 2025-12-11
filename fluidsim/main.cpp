1d31e40bee43dd9524bd72738f83e770b49a9972
Alex Tavares <144470569+NoiceFrog@users.noreply.github.com>
2025-12-11 18:25:42 +0100

    ngl kinda forgot all the features i added

--- fluidsim/main.cpp
+++ fluidsim/main.cpp
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <deque>
 
 #define GL_SILENCE_DEPRECATION
 using namespace std;
@@ -31,8 +32,6 @@ static uint32_t rrggbb_to_aabbggrr(uint32_t u24_tracing_color) {
         | (u24_tracing_color & 0xff) << 16;
 }
 
-// test bonjour
-
 GLFWwindow* window;
 
 void reset();
@@ -42,12 +41,12 @@ float dt;
 float currentTime;
 
 float gravity = 0.0;
+float separationForce = 2000.0;
 float radius = 3.0f;
 
-float startingX = 5.0;
-float startingY = 710.0;
-float startingVX = 0.0;
-float startingVY = 0.0;
+float startingX = 5.0, startingY = 710.0;
+float startingVX = 0.0, startingVY = 0.0;
+float VX = startingVX, VY = startingVY;
 float startingColorR = 0.31;
 float startingColorG = 0.62;
 float startingColorB = 0.62;
@@ -64,6 +63,12 @@ bool resetSimButton;
 
 bool show_demo_window = false;
 bool show_implot_demo_window = false;
+
+int particle_ammount;
+
+int maxFPSbufSamples = 300;
+
+static float col1[3] = { 1.0f, 0.0f, 0.2f };
 
 class Particle {
 public:
@@ -143,7 +148,6 @@ void CollisionHandler(float dt) {
             auto distance = sqrt(((dx) * (dx)) + ((dy) * (dy)));
             
             if (distance < radius * 2) {
-                float separationForce = 1000.0f;
                 //a.vx -= separationForce * dt;
                 //a.vy -= separationForce * dt;
                 //b.vx += separationForce * dt;
@@ -182,30 +186,27 @@ void CollisionHandler(float dt) {
 }
 
 void particleCreation() {
-    for (int i = 0; i < 0; i++) {
-        particles.push_back(Particle{ startingX, startingY - 10, startingVX, -100, 1.0, 0.0, 0.0});
+    for (int n = 0; n < particle_ammount; n++) {
+        particles.push_back(Particle{ startingX, startingY, VX, VY, startingColorR, startingColorG, startingColorB});
         //startingX += 5;
-        //startingVX++;
-        //startingVY++;
-    }
-    startingX = 5;
-    startingY -= 10;
-    for (int n = 0; n < 5000; n++) {
-        particles.push_back(Particle{ startingX, startingY, startingVX, startingVY, startingColorR, startingColorG, startingColorB});
-        //startingX += 5;
-        startingX += 5;
-        startingVX++;
-        startingVY++;
+        startingX++;
+        VX++;
+        VY++;
     }
 }
 
 /* TODO: fix colors */
 void changeColor() {
     for (auto& p : particles) {
-        if (p.vx > 100.0 || p.vy > 100.0) {
-            p.r = p.vy;
-            p.g = p.vy;
-            p.b = p.vy;
+        if (p.vx > 300.0 || p.vy > 300.0) {
+            p.r = col1[0];
+            p.g = col1[1];
+            p.b = col1[2];
+        }
+        else if (p.vx > 300.0 && p.vy > 300.0) {
+            p.r = col1[0];
+            p.g = col1[1];
+            p.b = col1[2];
         }
         else if (p.vx < 100.0 || p.vy < 100.0) {
             p.r = startingColorR;
@@ -229,6 +230,7 @@ void ImguiWindow(ImGuiIO& io = ImGui::GetIO()) {
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
+
     if (show_demo_window) {
         ImGui::ShowDemoWindow(&show_demo_window);
     }
@@ -236,58 +238,75 @@ void ImguiWindow(ImGuiIO& io = ImGui::GetIO()) {
         ImPlot::ShowDemoWindow();
     }
 
-    static float f = 0.0f;
-    static int counter = 0;
+    ImGui::Begin("Particle Simulator");                          // Create a window called "Hello, world!" and append into it.
 
-    ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-    ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
     // Edit bools storing our window open/close state
     //ImGui::Checkbox("Another Window", &show_another_window);
 
-    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-    //ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+    ImGui::SliderInt("Ammount of particles", &particle_ammount, 0, 5000);
+    ImGui::Spacing();
+    ImGui::Spacing();
+    ImGui::SliderFloat("Gravity", &gravity, 0.0f, 5000.0f);
+    ImGui::Spacing();
+    ImGui::Spacing();
+    ImGui::SliderFloat("Separation Force", &separationForce, 0.0, 5000.0f);
+    ImGui::Spacing();
+    ImGui::Spacing();
+    ImGui::SliderFloat("Starting Velocity X", &startingVX, -1000.0, 1000.0f);
+    ImGui::Spacing();
+    ImGui::SliderFloat("Starting Velocity Y", &startingVY, -1000.0, 1000.0f);
+    ImGui::Spacing();
+    ImGui::Spacing();
+    ImGui::ColorEdit3("clear color", col1); // Edit 3 floats representing a color
+    ImGui::Spacing();
+    //ImGui::SliderFloat("Starting Velocity Y", &separationForce, 0.0, 5000.0f);
     /*
-    auto size = ImVec2(ImGui::GetWindowSize().x - 20, 200 * 1.0);
-    if (ImPlot::BeginPlot("Timing", size, ImPlotFlags_NoInputs)) {
-        static const ImU32 color_map_data[] = {
-            rrggbb_to_aabbggrr(TRACING_COLOR_MAP_UPDATE),
-            rrggbb_to_aabbggrr(TRACING_COLOR_RENDER),
-        };
-        static ImPlotColormap timing_color_map = -1;
-        if (timing_color_map == -1) {
-            timing_color_map = ImPlot::AddColormap("CycleTimesUpdateTimeColorMap", color_map_data, NUM_ELEM(color_map_data));
+    if (ImGui::Begin("fps")) {
+        auto size = ImVec2(ImGui::GetWindowSize().x - 20, 200 * 1.0);
+        if (ImPlot::BeginPlot("Timing", size, ImPlotFlags_NoInputs)) {
+            static const ImU32 color_map_data[] = {
+                rrggbb_to_aabbggrr(TRACING_COLOR_MAP_UPDATE),
+                rrggbb_to_aabbggrr(TRACING_COLOR_RENDER),
+            };
+            static ImPlotColormap timing_color_map = -1;
+            if (timing_color_map == -1) {
+                timing_color_map = ImPlot::AddColormap("CycleTimesUpdateTimeColorMap", color_map_data, NUM_ELEM(color_map_data));
+            }
+
+            ImPlot::PushColormap(timing_color_map);
+
+            //ImPlot::SetupAxes("sample", "time (ms)");
+            //ImPlot::PlotLine("map_update", map_update_x, map_update_y, total_samples, 0, 0);
+            //ImPlot::PlotLine("framerate", fpsBuf., fpsBuf.size());
+
+            ImPlot::PopColormap();
+
+            ImPlot::EndPlot();
         }
-
-        ImPlot::PushColormap(timing_color_map);
-
-        ImPlot::SetupAxes("sample", "time (ms)");
-        //ImPlot::PlotLine("map_update", map_update_x, map_update_y, total_samples, 0, 0);
-        //ImPlot::PlotLine("render", render_x, render_y, total_samples, 0, 0);
-
-        ImPlot::PopColormap();
-
-        ImPlot::EndPlot();
-    }
-    */
+        ImGui::End();
+    } */
+    
 
     if (ImGui::Button("reset")) { resetSimButton = true; };
     ImGui::Checkbox("imgui demo window", &show_demo_window);
     ImGui::Checkbox("implot demo window", &show_implot_demo_window);
 
+    ImGui::TextLinkOpenURL("Github repo", "https://github.com/anAndjel/Particle-Fluid-simulation");
+    ImGui::Spacing();
+    ImGui::TextLinkOpenURL("Buy me a coffee!", "https://buymeacoffee.com/alexmtavares?new=1");
+    
     ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
     ImGui::End();
 }
 
 void reset() {
-    std::cout << "reset" << '\n';
     lastTime = glfwGetTime();
     dt = 0;
     currentTime = 0;
     startingX = 5.0;
     startingY = 710.0;
-    startingVX = 0.0;
-    startingVY = 0.0;
+    VX = startingVX;
+    VY = startingVY;
     startingColorR = 0.31;
     startingColorG = 0.62;
     startingColorB = 0.62;
@@ -295,7 +314,6 @@ void reset() {
     while (!particles.empty()) {
         particles.pop_back();
     }
-
 
     particleCreation();
     resetSimButton = false;
@@ -316,6 +334,7 @@ int main() {
     }
 
     glfwMakeContextCurrent(window);
+    //glfwSwapInterval(1); // Disable vsync
 
     // Decide GL+GLSL versions
 #if defined(IMGUI_IMPL_OPENGL_ES2)
@@ -411,6 +430,7 @@ int main() {
 
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
+    ImPlot::DestroyContext();
     ImGui::DestroyContext();
 
     glfwDestroyWindow(window);
